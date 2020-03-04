94bf2129ccfd837bb98719c067302e8714bd25f6
Joseph Lukan <ztobscieng@gmail.com>
2020-03-04 21:25:01 +0100

    Updates

---
+++ precache-manifest.57a89d6186d0374c26f50ffa74f83291.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9de67dc5ddfdc10cf6c31f0baf41544",
+    "url": "/cm/index.html"
+  },
+  {
+    "revision": "40aee54bbac2cd7b5c0f",
+    "url": "/cm/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "8fc7722b16029d3c91f8",
+    "url": "/cm/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "40aee54bbac2cd7b5c0f",
+    "url": "/cm/static/js/2.524dcc7f.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/cm/static/js/2.524dcc7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fc7722b16029d3c91f8",
+    "url": "/cm/static/js/main.e1c789d0.chunk.js"
+  },
+  {
+    "revision": "9fe49aa30c7535501e9c",
+    "url": "/cm/static/js/runtime-main.cd749980.js"
+  }
+]);
