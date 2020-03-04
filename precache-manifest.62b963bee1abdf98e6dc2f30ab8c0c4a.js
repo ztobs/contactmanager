3a7a8226c8f325df62579ebd96943d40418bc0fc
Joseph Lukan <ztobscieng@gmail.com>
2020-03-04 21:26:48 +0100

    Updates

---
+++ precache-manifest.62b963bee1abdf98e6dc2f30ab8c0c4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb2874b346b31f23f4ced08ea5c11c4b",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "40aee54bbac2cd7b5c0f",
+    "url": "/contactmanager/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "628efa04b6f01ce3cf16",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "40aee54bbac2cd7b5c0f",
+    "url": "/contactmanager/static/js/2.524dcc7f.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/contactmanager/static/js/2.524dcc7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "628efa04b6f01ce3cf16",
+    "url": "/contactmanager/static/js/main.ef8d0fff.chunk.js"
+  },
+  {
+    "revision": "8cb2c909a997ad49a72a",
+    "url": "/contactmanager/static/js/runtime-main.fce9aea4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cm/precache-manifest.57a89d6186d0374c26f50ffa74f83291.js"
+  "/contactmanager/precache-manifest.62b963bee1abdf98e6dc2f30ab8c0c4a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cm/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/contactmanager/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
